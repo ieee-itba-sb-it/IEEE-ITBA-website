8b01b52ff84e93789c7e671a1163549187900a88
Euge <epineiro@itba.edu.ar>
2020-08-16 21:03:32 -0300

    added image and author input fields

--- src/app/write-news/write-news.component.ts
+++ src/app/write-news/write-news.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { BlogService } from '../blog.service';
+import { ActivatedRoute } from '@angular/router';
+import { firestore } from 'firebase/app';
+import { Router } from "@angular/router";
+import { newsItem, createNewsItem } from '../data-types';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
 
 @Component({
   selector: 'app-write-news',
@@ -10,10 +16,52 @@ export class WriteNewsComponent implements OnInit {
 
   editorForm: FormGroup;
 
+
+  constructor(private router: Router, private route: ActivatedRoute, private blogService: BlogService) { }
+
   ngOnInit() {
     this.editorForm = new FormGroup({
       'editor': new FormControl(null)
     })
   }
+
+  submitNews() {
+
+    var content = document.getElementsByClassName('ql-editor')[0].innerHTML;
+    console.log(content);
+    var title = (<HTMLInputElement>document.getElementById("title")).value;
+    var shortIntro = (<HTMLInputElement>document.getElementById("shortIntro")).value;
+    var author = (<HTMLInputElement>document.getElementById("author")).value;
+    var imageUrl = (<HTMLInputElement>document.getElementById("imageUrl")).value;
+
+    /* if (title != '') {
+       this.blogService.setDoc(
+         createNewsItem(
+           title,
+           content,
+           shortIntro,
+           imageUrl,
+           Timestamp.fromDate(new Date()),
+           author,
+           imageText,
+           reference,
+           null,
+           null
+         )
+       ).subscribe(sent => {
+         if (sent) {
+           this.router.navigate([`/anuncios/${reference}`]);
+         }
+       })
+     } else {
+       this.blogService.deleteDoc(reference).subscribe(sent => {
+         if (sent) {
+           this.router.navigate([`/anuncios`]);
+         }
+       });
+     }*/
+  }
+
+
 
 }
