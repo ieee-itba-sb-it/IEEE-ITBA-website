974f7332a2207b64baf029080c3201ba05290797
euge <epineiro@itba.edu.ar>
2021-03-22 23:05:32 -0300

    changed login frontend

--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -4,12 +4,19 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule
   ]
 })
 export class LoginModule { }
