01fa8bf4b66bdba0ba37176c6233629f53401f5b
mricarte <mricarte@itba.edu.ar>
2021-09-25 00:22:48 -0300

    typo on comment

--- src/app/modules/write-news/utils.ts
+++ src/app/modules/write-news/utils.ts
@@ -3,7 +3,7 @@ export function sanitizeString(input: string) : string {
     let output: string;
     output = input.toLowerCase();
 
-    //taking care of diatrical signs (tildes, otros signos que se agregan a las letras)
+    //taking care of diacritical signs (tildes, otros signos que se agregan a las letras)
     output = output.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
     //clean up troublesome characters (remplaza todo caracter que no sea digito, letra, guion bajo (\w), espacio por vacio (\s), o sea las quita)
     output = output.replace(/[^\w\s]/gi, '')
