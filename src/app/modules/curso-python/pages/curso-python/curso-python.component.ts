563b2d5db11ae3e87ae4117cc45a98b7e6b16d91
nrooney99 <84242801+nrooney99@users.noreply.github.com>
2022-05-07 12:00:18 -0300

    Changed upload dates

--- src/app/modules/curso-python/pages/curso-python/curso-python.component.ts
+++ src/app/modules/curso-python/pages/curso-python/curso-python.component.ts
@@ -1,8 +1,9 @@
 /*IMPORTS*/
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
-
+import firebase from 'firebase';
+import { firestore } from 'firebase/app';
+import Timestamp = firestore.Timestamp;
 @Component({
   selector: 'app-curso-python',
   templateUrl: './curso-python.component.html',
@@ -10,20 +11,48 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class CursoPythonComponent implements OnInit {
 
-  // ojala no tuviera que hardcodear asi, pero no encontre una forma de pasarle el string y concatenarlo con otros en el translate
+
+  class1Open: boolean = false;
+  class2Open: boolean = false;
+  class3Open: boolean = false;
+
+  //ojala no tuviera que hardcodear asi, pero no encontre una forma de pasarle el string y concatenarlo con otros en el translate
   faq = [
-    {q: 'PYTHONCOURSES.FAQ.1.QUESTION', a: 'PYTHONCOURSES.FAQ.1.ANSWER'},
-    {q: 'PYTHONCOURSES.FAQ.2.QUESTION', a: 'PYTHONCOURSES.FAQ.2.ANSWER'},
-    {q: 'PYTHONCOURSES.FAQ.3.QUESTION', a: 'PYTHONCOURSES.FAQ.3.ANSWER'},
-    {q: 'PYTHONCOURSES.FAQ.4.QUESTION', a: 'PYTHONCOURSES.FAQ.4.ANSWER'},
-    {q: 'PYTHONCOURSES.FAQ.5.QUESTION', a: 'PYTHONCOURSES.FAQ.5.ANSWER'},
-  ];
+    {q:'PYTHONCOURSES.FAQ.1.QUESTION', a:'PYTHONCOURSES.FAQ.1.ANSWER'},
+    {q:'PYTHONCOURSES.FAQ.2.QUESTION', a:'PYTHONCOURSES.FAQ.2.ANSWER'},
+    {q:'PYTHONCOURSES.FAQ.3.QUESTION', a:'PYTHONCOURSES.FAQ.3.ANSWER'},
+    {q:'PYTHONCOURSES.FAQ.4.QUESTION', a:'PYTHONCOURSES.FAQ.4.ANSWER'},
+    {q:'PYTHONCOURSES.FAQ.5.QUESTION', a:'PYTHONCOURSES.FAQ.5.ANSWER'},
+  ]
+
+  tyc = [
+    {title:'TYC.SUBTITLES.1.TITLE',text:'TYC.SUBTITLES.1.TEXT'},
+    {title:'TYC.SUBTITLES.2.TITLE',text:'TYC.SUBTITLES.2.TEXT'},
+    {title:'TYC.SUBTITLES.3.TITLE',text:'TYC.SUBTITLES.3.TEXT'},
+    {title:'TYC.SUBTITLES.4.TITLE',text:'TYC.SUBTITLES.4.TEXT'},
+  ]
+
+  getDate () {
+  console.log()
+  }
+
+  isOldDate(date: string){
+    this.getDate()
+    let oldDate = Timestamp.fromDate(new Date(date));
+    let now = firebase.firestore.Timestamp.now()
+    return now > oldDate ;
+  }
+
+
   constructor(public translate: TranslateService) {
     scroll(0, 0);
     translate.addLangs(['en', 'es']);
     translate.setDefaultLang('es');
     const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/es|en/) ? browserLang : 'en');
+    translate.use(browserLang.match(/es|en/)? browserLang:'en');
+    this.class1Open = this.isOldDate('31 Apr 2022 14:00:00 UTC');
+    this.class2Open = this.isOldDate('07 May 2022 14:00:00 UTC');
+    this.class3Open = this.isOldDate('14 May 2022 15:00:00 UTC');
   }
   useLanguage(language: string) {
     this.translate.use(language);
