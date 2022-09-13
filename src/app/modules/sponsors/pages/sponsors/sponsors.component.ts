7c45a0a6e02ee1610db0d4ef81888ed79b0ba2e9
Fegrant <nicolasbirsa333@gmail.com>
2022-09-13 20:19:40 -0300

    feat(sponsorsPage): add ieeextreme sponsors

--- src/app/modules/sponsors/pages/sponsors/sponsors.component.ts
+++ src/app/modules/sponsors/pages/sponsors/sponsors.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SponsorsComponent implements OnInit {
 
+  imgSrcPrefix = '../../../../../assets/image/content/';
+
+  currentSponsors = [
+    {
+      name: 'JPMorgan',
+      img: this.imgSrcPrefix + 'jpmorgan.png'
+    },
+    {
+      name: 'Schneider Electric',
+      img: this.imgSrcPrefix + 'Schneider Logo.png'
+    },
+    {
+      name: 'Innovid',
+      img: this.imgSrcPrefix + 'innovid.png'
+    },
+    {
+      name: 'Inclusion',
+      img: this.imgSrcPrefix + 'inclusion.png'
+    },
+    {
+      name: 'Hitachi Energy',
+      img: this.imgSrcPrefix + 'hitachi-energy.png'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
