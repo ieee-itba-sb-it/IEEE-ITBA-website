7ad704bc207f22d2dc366c0a23655d7a7e0dda6e
Fegrant <nicolasbirsa333@gmail.com>
2022-09-23 13:45:01 -0300

    feat: add about us page

--- src/app/modules/about-us/about-us.module.ts
+++ src/app/modules/about-us/about-us.module.ts
@@ -9,26 +9,30 @@ import { HttpClient } from "@angular/common/http";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatCardModule } from "@angular/material/card";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatTabsModule} from '@angular/material/tabs';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 const routes: Routes = [{ path: "", component: AboutUsComponent }];
 export const routing = RouterModule.forChild(routes);
 
 @NgModule({
-  imports: [
-    routing,
-    CommonModule,
-    SharedModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
-    MatExpansionModule,
-    MatCardModule,
-    FlexLayoutModule,
-  ],
+    imports: [
+        routing,
+        CommonModule,
+        SharedModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }),
+        MatExpansionModule,
+        MatCardModule,
+        FlexLayoutModule,
+        MatTabsModule,
+        MDBBootstrapModule,
+    ],
   declarations: [AboutUsComponent],
 })
 export class AboutUsModule {}
--- src/app/modules/about-us/pages/about-us/about-us.component.ts
+++ src/app/modules/about-us/pages/about-us/about-us.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: "app-about-us",
   templateUrl: "./about-us.component.html",
-  styleUrls: ["./about-us.component.css"],
+  styleUrls: ["./about-us.component.css"]
 })
+
 export class AboutUsComponent implements OnInit {
+
   constructor(public translate: TranslateService) {
     scroll(0, 0);
     translate.addLangs(["en", "es"]);
