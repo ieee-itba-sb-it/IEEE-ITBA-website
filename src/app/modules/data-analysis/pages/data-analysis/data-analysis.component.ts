6dde762f593ff1a249d2324202a7ff1348e135d3
Fegrant <nicolasbirsa333@gmail.com>
2022-10-02 21:18:45 -0300

    feat(dataAnalysisCourse): add test links

--- src/app/modules/data-analysis/pages/data-analysis/data-analysis.component.ts
+++ src/app/modules/data-analysis/pages/data-analysis/data-analysis.component.ts
@@ -21,11 +21,7 @@ export class DataAnalysisComponent implements OnInit {
     {q: 'DATAANALYSIS.FAQ.4.QUESTION', a: 'DATAANALYSIS.FAQ.4.ANSWER'},
     {q: 'DATAANALYSIS.FAQ.5.QUESTION', a: 'DATAANALYSIS.FAQ.5.ANSWER'},
   ];
-isOldDate(date: string) {
-    let oldDate = Timestamp.fromDate(new Date(date));
-    let now = firebase.firestore.Timestamp.now();
-    return now > oldDate;
-  }
+
   constructor(public translate: TranslateService) {
   scroll(0, 0);
   translate.addLangs(['en', 'es']);
@@ -35,6 +31,45 @@ isOldDate(date: string) {
   this.class1Open = this.isOldDate("19 Sep 2022 03:00:00 UTC");
   this.class2Open = this.isOldDate("26 Sep 2022 03:00:00 UTC");
   this.class3Open = this.isOldDate("03 Oct 2022 03:00:00 UTC");
+}
+
+isOldDate(date: string) {
+    let oldDate = Timestamp.fromDate(new Date(date));
+    let now = firebase.firestore.Timestamp.now();
+    return now > oldDate;
+  }
+
+getCurrentTest() {
+  let now = firebase.firestore.Timestamp.now();
+  if (now > Timestamp.fromDate(new Date('09 Oct 2022 03:00:00 UTC'))) {
+    return 'https://forms.gle/vd3ado6TurViy67b9';
+  }
+  else if (now > Timestamp.fromDate(new Date('08 Oct 2022 03:00:00 UTC'))) {
+    return 'https://forms.gle/5r8dmz2vAvUgXUbm9';
+  }
+  else if (now > Timestamp.fromDate(new Date('07 Oct 2022 03:00:00 UTC'))) {
+    return 'https://forms.gle/wCRksx3hA1QiPazx9';
+  }
+  else if (now > Timestamp.fromDate(new Date('06 Oct 2022 03:00:00 UTC'))) {
+    return 'https://forms.gle/ir4mXqLooAYx4oV76';
+  }
+  else if (now > Timestamp.fromDate(new Date('05 Oct 2022 03:00:00 UTC'))) {
+    return 'https://forms.gle/2dYjc3B4FRZqj4nD6';
+  }
+  else if (now > Timestamp.fromDate(new Date('04 Oct 2022 03:00:00 UTC'))) {
+    return 'https://forms.gle/FZEsP5FaszstpCj57';
+  }
+  return 'https://forms.gle/Btg5eaZDtfQpZDUdA';
+}
+
+hasTestsAvailable() {
+    let now = firebase.firestore.Timestamp.now();
+    let startDate = Timestamp.fromDate(new Date('03 Oct 2022 03:00:00 UTC'));
+    let endDate = Timestamp.fromDate(new Date('10 Oct 2022 03:00:00 UTC'));
+    if (startDate < now && now < endDate) {
+      return true;
+    }
+    return false;
 }
 
 useLanguage(language: string) {
