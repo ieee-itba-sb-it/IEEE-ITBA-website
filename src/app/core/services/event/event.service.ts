ea48156b8d17c2145593b7ce1f6547ff4b7c6e28
sh1ft <31477929+Federico-Shih@users.noreply.github.com>
2023-05-30 23:14:07 -0300

    feat: upcoming events section on mainpage
    * feat: add homepage events
    
    * feat: event card changes for previous date
    
    * chore: changing styles
    
    * feat: add event-card-short-component
    
    * chore: simplify date
    
    * chore: add typescript date
    
    * chore: use IEEE colors and remove unused styles
    
    * chore: updated events' dates and capitalized months
    
    ---------
    
    Co-authored-by: Fegrant <nicolasbirsa333@gmail.com>

--- src/app/core/services/event/event.service.ts
+++ src/app/core/services/event/event.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { EventCardData } from '../../../shared/models/event/event-card-data';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -7,31 +8,121 @@ import { EventCardData } from '../../../shared/models/event/event-card-data';
 
 export class EventService {
 
-  private ieeextreme: EventCardData = { routerLink: '/ieeextreme', imageSrc: '../../../../../assets/image/events/ieeextreme/ieee-extreme.jpg',
-    imageAlt: 'Foto de competicion IEEExtreme', titleCode: 'HOME.IEEEXTREME.TITLE', descriptionCode: 'HOME.IEEEXTREME.TEXT'};
+  private ieeextreme: EventCardData = {
+    routerLink: '/ieeextreme',
+    imageSrc: '../../../../../assets/image/events/ieeextreme/ieee-extreme.jpg',
+    imageAlt: 'Foto de competicion IEEExtreme',
+    titleCode: 'HOME.IEEEXTREME.TITLE',
+    descriptionCode: 'HOME.IEEEXTREME.TEXT',
+    dates: [
+      {
+        date: new Date(2023, 9, 28),
+        descriptionCode: 'EVENTCARD.DATE.TBD'
+      }
+    ]
+  };
 
-  private cursosPython: EventCardData = { routerLink: '/cursospython', imageSrc: '../../../../../assets/image/events/python-introductory/cursopython.jpg',
-    imageAlt: 'Foto de los cursos de Python', titleCode: 'HOME.CLASSES.TITLE', descriptionCode: 'HOME.CLASSES.TEXT'};
+  private cursosPython: EventCardData = {
+    routerLink: '/cursospython',
+    imageSrc: '../../../../../assets/image/events/python-introductory/cursopython.jpg',
+    imageAlt: 'Foto de los cursos de Python',
+    titleCode: 'HOME.CLASSES.TITLE',
+    descriptionCode: 'HOME.CLASSES.TEXT',
+    dates: [
+      {
+        date: new Date(2023, 7, 7),
+        descriptionCode: 'EVENTCARD.DATE.TBD',
+      }
+    ]
+  };
 
-  private bitcup: EventCardData = { routerLink: '/bitcup', imageSrc: '../../../../../assets/image/events/bitcup/bitcup-logo.jpeg',
-    imageAlt: 'Foto de los cursos de Python', titleCode: 'HOME.BITCUP.TITLE', descriptionCode: 'HOME.BITCUP.TEXT'};
+  private bitcup: EventCardData = {
+    routerLink: '/bitcup',
+    imageSrc: '../../../../../assets/image/events/bitcup/bitcup-logo.jpeg',
+    imageAlt: 'Foto de los cursos de Python',
+    titleCode: 'HOME.BITCUP.TITLE',
+    descriptionCode: 'HOME.BITCUP.TEXT',
+    dates: []
+  };
 
-  private dataAnalysis: EventCardData = { routerLink: '/data-analysis', imageSrc: '../../../../../assets/image/events/data-analysis/data-analysis-event.png',
-    imageAlt: 'Foto de curso analisis de datos', titleCode: 'HOME.DATAANALYSIS.TITLE', descriptionCode: 'HOME.DATAANALYSIS.TEXT'};
+  private dataAnalysis: EventCardData = {
+    routerLink: '/data-analysis',
+    imageSrc: '../../../../../assets/image/events/data-analysis/data-analysis-event.png',
+    imageAlt: 'Foto de curso analisis de datos',
+    titleCode: 'HOME.DATAANALYSIS.TITLE',
+    descriptionCode: 'HOME.DATAANALYSIS.TEXT',
+    dates: [
+      {
+        date: new Date(2023, 8, 18),
+        descriptionCode: '',
+      }
+    ]
+  };
 
-  private asimovCup: EventCardData = { routerLink: '/asimovcup', imageSrc: '../../../../../assets/image/events/asimov-cup/asimov-cup-logo.png',
-    imageAlt: 'Logo de la Asimov Cup', titleCode: 'HOME.ASIMOVCUP.TITLE', descriptionCode: 'HOME.ASIMOVCUP.TEXT'};
+  private asimovCup: EventCardData = {
+    routerLink: '/asimovcup',
+    imageSrc: '../../../../../assets/image/events/asimov-cup/asimov-cup-logo.png',
+    imageAlt: 'Logo de la Asimov Cup',
+    titleCode: 'HOME.ASIMOVCUP.TITLE',
+    descriptionCode: 'HOME.ASIMOVCUP.TEXT',
+    dates: [
+      {
+        date: new Date(2023, 6, 29),
+        descriptionCode: ''
+      }
+    ]
+  };
 
-  private iotWorkshop: EventCardData = { routerLink: '/iot', imageSrc: '../../../../../assets/image/events/iot/iot-banner.jpeg',
-    imageAlt: 'Logo de la Asimov Cup', titleCode: 'HOME.IOT.TITLE', descriptionCode: 'HOME.IOT.TEXT'};
+  private iotWorkshop: EventCardData = {
+    routerLink: '/iot',
+    imageSrc: '../../../../../assets/image/events/iot/iot-banner.jpeg',
+    imageAlt: 'Logo de la Asimov Cup',
+    titleCode: 'HOME.IOT.TITLE',
+    descriptionCode: 'HOME.IOT.TEXT',
+    dates: []
+  };
 
-  private typescript: EventCardData = { routerLink: '/curso-typescript', imageSrc: '../../../../../assets/image/events/typescript-course/banner.jpg',
-    imageAlt: 'Foto de curso de Typescript', titleCode: 'TYPESCRIPT.TITLE', descriptionCode: 'TYPESCRIPT.DESCRIPTION.TEXT'};
+  private typescript: EventCardData = {
+    routerLink: '/curso-typescript',
+    imageSrc: '../../../../../assets/image/events/typescript-course/banner.jpg',
+    imageAlt: 'Foto de curso de Typescript',
+    titleCode: 'TYPESCRIPT.TITLE',
+    descriptionCode: 'TYPESCRIPT.DESCRIPTION.TEXT',
+    dates: [{
+      date: new Date(2023, 4, 31),
+      descriptionCode: '',
+    }]
+  };
 
   constructor() { }
 
   getAllEvents(): EventCardData[] {
-    return [ this.ieeextreme, this.cursosPython, this.bitcup, this.dataAnalysis, this.asimovCup, this.iotWorkshop, this.typescript];
+    return [this.ieeextreme,
+      this.cursosPython,
+      this.bitcup,
+      this.dataAnalysis,
+      this.asimovCup,
+      this.iotWorkshop,
+      this.typescript];
+  }
+
+  getUpcomingEvents(): EventCardData[] {
+    return this.getAllEvents()
+      .filter((event) => event.dates.length > 0 && (event.dates[0].date == null || event.dates[0].date.getTime() >= firebase.firestore.Timestamp.now().toDate().getTime()))
+      .sort((event1, event2) => {
+      if (event1.dates.length !== event2.dates.length) {
+        return event2.dates.length - event1.dates.length;
+      }
+      const eventDate1 = event1.dates[0];
+      const eventDate2 = event2.dates[0];
+      if (eventDate1.date && eventDate2.date) {
+        if (eventDate1.showMonth && eventDate2.showMonth || !eventDate1.showMonth && !eventDate2.showMonth) {
+          return eventDate1.date.getTime() - eventDate2.date.getTime();
+        }
+        return eventDate1.showMonth ? 1 : -1;
+      }
+      return event1.dates[0].date ? -1 : 1;
+    });
   }
 
   getRasEvents(): EventCardData[] {
