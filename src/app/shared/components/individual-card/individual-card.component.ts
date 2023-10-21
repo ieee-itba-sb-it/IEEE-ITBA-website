75fe7e30dd052429d06eb9ec5b0524bb5813708f
sh1ft <31477929+Federico-Shih@users.noreply.github.com>
2023-10-21 18:14:17 -0300

    chore: load team to db (#58)
    * chore: load team information to db
    
    * chore: more roles
    
    * fix: remove unused comments, sorting and final implementation
    
    * fix: forgot to add enums and user response. Remove wrong css.

--- src/app/shared/components/individual-card/individual-card.component.ts
+++ src/app/shared/components/individual-card/individual-card.component.ts
@@ -1,5 +1,6 @@
-import { TeamMember } from '../../models/team-member';
-import { Component, Input, OnInit } from '@angular/core';
+import {TeamMember} from '../../models/team-member';
+import {Component, Input, OnInit} from '@angular/core';
+
 @Component({
     selector: 'app-individual-card',
     templateUrl: './individual-card.component.html',
@@ -18,5 +19,4 @@ export class IndividualCardComponent implements OnInit {
   shouldNotShowPerson() {
       return !!this.person && this.person.name === 'white';
   }
-
 }
