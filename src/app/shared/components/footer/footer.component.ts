283e54e88d8274654a22261640c21051044047a0
Saul Castañeda <scastaneda@itba.edu.ar>
2023-11-30 00:02:46 -0300

    feat: improve footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
     constructor() { }
 
     ngOnInit(): void {
