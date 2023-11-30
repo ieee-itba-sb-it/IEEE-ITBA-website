c5bb1a98dc2bfa0023a420513dc995418d76d80a
sh1ft <31477929+Federico-Shih@users.noreply.github.com>
2023-11-30 18:31:32 -0300

    fix: add extra padding for pages and make footer depend on navbar color

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-footer',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
     constructor() { }
+
+    @Input() color: string = '#00629BFF';
 
     ngOnInit(): void {
     }
