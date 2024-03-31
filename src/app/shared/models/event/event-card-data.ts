54ff3e5b9e2835c009bb0ee9992c80f39aac7d42
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-03-31 18:11:38 -0300

    feat: create ieee event enum

--- src/app/shared/models/event/event-card-data.ts
+++ src/app/shared/models/event/event-card-data.ts
@@ -1,3 +1,13 @@
+export const IeeeEvent = {
+    IEEE_EXTREME: "IEEE_EXTREME",
+    PYTHON_COURSE: "PYTHON_COURSE",
+    BITCUP: "BITCUP",
+    DATA_ANALYSIS: "DATA_ANALYSIS",
+    ASIMOV_CUP: "ASIMOV_CUP",
+    IOT_WORKSHOP: "IOT_WORKSHOP",
+    TYPESCRIPT_COURSE: "TYPESCRIPT_COURSE",
+};
+
 export interface EventCardData {
   routerLink: string;
   imageSrc: string;
