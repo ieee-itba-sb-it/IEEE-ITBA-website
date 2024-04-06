ad8af9eb7883a0ef477df99c650e46bb32a12490
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-04-06 11:56:13 -0300

    chore: refactor IeeeEvent as enum

--- src/app/shared/models/event/event-card-data.ts
+++ src/app/shared/models/event/event-card-data.ts
@@ -1,15 +1,15 @@
-export const IeeeEvent = {
-    IEEE_EXTREME: "IEEE_EXTREME",
-    PYTHON_COURSE: "PYTHON_COURSE",
-    BITCUP: "BITCUP",
-    DATA_ANALYSIS: "DATA_ANALYSIS",
-    ASIMOV_CUP: "ASIMOV_CUP",
-    IOT_WORKSHOP: "IOT_WORKSHOP",
-    TYPESCRIPT_COURSE: "TYPESCRIPT_COURSE",
-};
+export enum IeeeEvent {
+    IEEE_EXTREME = "IEEE_EXTREME",
+    PYTHON_COURSE = "PYTHON_COURSE",
+    BITCUP = "BITCUP",
+    DATA_ANALYSIS = "DATA_ANALYSIS",
+    ASIMOV_CUP = "ASIMOV_CUP",
+    IOT_WORKSHOP = "IOT_WORKSHOP",
+    TYPESCRIPT_COURSE = "TYPESCRIPT_COURSE",
+}
 
 export interface EventCardData {
-  id: string;
+  id: IeeeEvent;
   routerLink: string;
   imageSrc: string;
   imageAlt: string;
