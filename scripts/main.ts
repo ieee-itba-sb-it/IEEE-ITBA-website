410db212d4ea078a2e81ab21e90e2836fae2b272
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-05-19 12:30:14 -0300

    chore: script that creates the first schema for events collection docs

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,16 @@
 import {generateMissingUsers} from "./clean-unauthenticated";
+import {createEventInitialSchema} from "./create-event-initial-schema";
+import {initializeApp} from "firebase-admin/app";
+
+//  export GOOGLE_APPLICATION_CREDENTIALS="/tmp/key.json" Prod
+// export FIREBASE_AUTH_EMULATOR_HOST='localhost:9099' for local
+// export FIRESTORE_EMULATOR_HOST='localhost:8080' for local
+initializeApp({
+    projectId: 'ieeeitba'
+});
 
 function main() {
-    generateMissingUsers();
+    createEventInitialSchema();
 }
 
 main();
