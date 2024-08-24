3313bdec6aacc29e463e545b5df0564d7e48d784
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-08-24 16:40:02 -0300

    chore: add argentina time methods

---
+++ src/app/shared/argentina-time.ts
@@ -0,0 +1,12 @@
+export const getArgentineDate = (date: Date): string => {
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+export const getArgentineTime = (date: Date): string => {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
