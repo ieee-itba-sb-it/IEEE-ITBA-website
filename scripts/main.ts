bce255b7fd38616ec0dd9fc2fe623b5fe6b1620f
Federico Shih <fedeshih@gmail.com>
2024-10-05 15:59:01 -0300

    feat: sponsors

--- scripts/main.ts
+++ scripts/main.ts
@@ -5,9 +5,9 @@ import {migrateEventCourses} from "./migrations/migrate-event-courses";
 
 //  export GOOGLE_APPLICATION_CREDENTIALS="/tmp/key.json" Prod
 // export FIREBASE_AUTH_EMULATOR_HOST='localhost:9099' for local
-process.env.FIREBASE_AUTH_EMULATOR_HOST='localhost:9099'
+// process.env.FIREBASE_AUTH_EMULATOR_HOST='localhost:9099'
 // export FIRESTORE_EMULATOR_HOST='localhost:8080' for local
-process.env.FIRESTORE_EMULATOR_HOST='localhost:8080'
+// process.env.FIRESTORE_EMULATOR_HOST='localhost:8080'
 initializeApp({
     projectId: 'ieeeitba'
 });
--- src/app/core/services/sponsors/sponsors.service.ts
+++ src/app/core/services/sponsors/sponsors.service.ts
@@ -21,10 +21,14 @@ export class SponsorsService {
             name: 'Yokogawa',
             img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fyokogawa.jpg?alt=media&token=300f278f-2e30-4964-b875-84ddccfdb2b0'
         },
-        // {
-        //     name: 'Le Wagon',
-        //     img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Flewagon.png?alt=media&token=8afe5950-44ab-4f5f-89aa-ad0c62e524b3'
-        // }
+        {
+            name: 'Le Wagon',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Flewagon2.png?alt=media&token=4096d5dd-f51e-4c59-bcab-043a8eaeb500'
+        },
+        {
+            name: 'Karpatkey',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fkarpatkey.png?alt=media&token=6ed477da-b0ff-471d-8936-b40bec4c848d'
+        }
     ];
 
     previousSponsors: Sponsor[] = [
@@ -87,10 +91,14 @@ export class SponsorsService {
             name: 'Yokogawa',
             img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fyokogawa.png?alt=media&token=4cad97a1-6525-431b-a9a4-3576c130efe5'
         },
-        // {
-        //     name: 'Le Wagon',
-        //     img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Flewagon.png?alt=media&token=8afe5950-44ab-4f5f-89aa-ad0c62e524b3'
-        // }
+        {
+            name: 'Le Wagon',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Flewagon2.png?alt=media&token=4096d5dd-f51e-4c59-bcab-043a8eaeb500'
+        },
+        {
+            name: 'Karpatkey',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fkarpatkey.png?alt=media&token=6ed477da-b0ff-471d-8936-b40bec4c848d'
+        }
     ];
 
     asimovSponsors: Sponsor[] = [];
