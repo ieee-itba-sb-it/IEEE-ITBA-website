800a186079a3e7f8f0c70bda9bd0377994f9882c
sh1ft <fedeshih@gmail.com>
2025-04-12 10:52:29 -0300

    fix: image links in iot

--- src/app/modules/iot/pages/iot/iot.component.ts
+++ src/app/modules/iot/pages/iot/iot.component.ts
@@ -14,12 +14,13 @@ export class IotComponent implements OnInit {
     event?: Event;
 
     imageLinks = [
-        'https://i.ibb.co/h2NXFqZ/image1.jpg',
-        'https://i.ibb.co/4NWyHH0/image2.jpg',
-        'https://i.ibb.co/WGdj7dn/image4.jpg',
-        'https://i.ibb.co/cLr94M0/image7.jpg',
-        'https://i.ibb.co/YTfrKfk/image3.jpg',
-        'https://i.ibb.co/HF074sP/image6.jpg'
+        'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2FDSC_5040.webp?alt=media&token=230dcff9-01bb-4d71-8a62-e2d7241c7112',
+        'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2FDSC_5042.webp?alt=media&token=ec9e7982-a5df-417d-bb48-393085860590',
+        'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2FIMG_4429.webp?alt=media&token=e6f19f8e-c707-41d0-8dfc-702b1681346f',
+        'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2FIMG_4449.webp?alt=media&token=36dc7cbc-6be4-4a24-aafc-cd4dc2eb3447',
+        'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2FIMG_4451.webp?alt=media&token=dbdbb96e-2221-4ad1-b240-bdb6c16bc930',
+        'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2FIMG_4444.jpg?alt=media&token=42bf91ac-c23b-46ff-8647-94669d86db7a',
+        'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2FIMG_4425.jpg?alt=media&token=08d4fc1d-d8e1-401a-be85-302985f33587'
     ];
 
     enrollLink = 'https://forms.gle/tWspqnnYahbbLVSF8';
@@ -39,6 +40,7 @@ export class IotComponent implements OnInit {
     constructor(private eventService: EventService, private seoService: StaticSeoService) { }
 
     ngOnInit(): void {
+        console.log("hi");
         this.seoService.updateMetaTags('IOT.PAGETITLE', 'IOT.PAGEDESCRIPTION', ['IOT', 'IEEE', 'ITBA'], "events/iot/iot-banner.jpeg");
         this.getEvent();
     }
