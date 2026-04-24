e950d8ca9a506b1d787826bdff3f74e0e2eff892
Emilio Mitchell <emitchell@itba.edu.ar>
2026-04-24 17:36:36 -0300

    chore(models): added exam-related models

---
+++ src/app/shared/models/event/data_analysis/exams.ts
@@ -0,0 +1,19 @@
+type Answer = {
+    answer: string;
+    isCorrect: boolean;
+    selected?: boolean;
+}
+
+export type Question = {
+    id: number;
+    question: string;
+    answers: Answer[];
+}
+
+export type SubmittedExam = {
+    user: string;
+    passed?: boolean;
+    submitted: boolean;
+    started: Date;
+    questions: Question[];
+}
