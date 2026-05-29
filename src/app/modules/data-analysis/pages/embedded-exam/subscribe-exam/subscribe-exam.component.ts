a72fe59e0ee597becda65bc76af8135723353c27
amparo19 <161982862+amparo19@users.noreply.github.com>
2026-05-29 11:41:34 -0300

    style: inscription form

--- src/app/modules/data-analysis/pages/embedded-exam/subscribe-exam/subscribe-exam.component.ts
+++ src/app/modules/data-analysis/pages/embedded-exam/subscribe-exam/subscribe-exam.component.ts
@@ -4,15 +4,23 @@ import {Router} from "@angular/router";
 import {EventService} from 'src/app/core/services/event/event.service';
 import {AuthService} from 'src/app/core/services/authorization/auth.service';
 import {IEEEuser} from "../../../../../shared/models/ieee-user/ieee-user";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
     selector: 'app-subscribe-exam',
     templateUrl: './subscribe-exam.component.html',
     standalone: true,
+    imports: [
+        MatFormFieldModule,
+        MatCardModule,
+        MatInputModule
+    ],
     styleUrls: ['./subscribe-exam.component.css']
 })
 export class SubscribeExamComponent implements OnInit {
-
+    today = new Date().toLocaleString('es-AR');
     user: IEEEuser | null = null;
 
     constructor(
