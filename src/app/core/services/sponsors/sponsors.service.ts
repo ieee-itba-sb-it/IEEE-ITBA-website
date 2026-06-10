f0736204c8a1d1665d8d365897bad413db4cc387
Emilio Mitchell <emitchell@itba.edu.ar>
2026-06-10 18:55:04 -0300

    chore(sponsors): updated sponsors

--- src/app/core/services/sponsors/sponsors.service.ts
+++ src/app/core/services/sponsors/sponsors.service.ts
@@ -10,24 +10,8 @@ export class SponsorsService {
 
     currentSponsors: Sponsor[] = [
         {
-            name: 'Innovid',
-            img: this.imgSrcPrefix + 'innovid.png'
-        },
-        {
-            name: 'DevRev',
-            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fdevrev-logo_black.png?alt=media&token=d5eb5bc7-3862-4af7-98f1-2223d4920625'
-        },
-        {
-            name: 'Yokogawa',
-            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fyokogawa.jpg?alt=media&token=300f278f-2e30-4964-b875-84ddccfdb2b0'
-        },
-        {
-            name: 'Le Wagon',
-            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Flewagon2.png?alt=media&token=4096d5dd-f51e-4c59-bcab-043a8eaeb500'
-        },
-        {
-            name: 'Karpatkey',
-            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fkarpatkey.png?alt=media&token=6ed477da-b0ff-471d-8936-b40bec4c848d'
+            name: 'Globant',
+            img: 'https://mqipggrhfgjyfgnzgphy.supabase.co/storage/v1/object/public/IEEE-ITBA-website/sponsors/Globant-Original.png',
         }
     ];
 
@@ -75,6 +59,26 @@ export class SponsorsService {
         {
             name: 'Schneider Electric',
             img: this.imgSrcPrefix + 'Schneider Logo.png'
+        },
+        {
+            name: 'Innovid',
+            img: this.imgSrcPrefix + 'innovid.png'
+        },
+        {
+            name: 'DevRev',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fdevrev-logo_black.png?alt=media&token=d5eb5bc7-3862-4af7-98f1-2223d4920625'
+        },
+        {
+            name: 'Yokogawa',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fyokogawa.jpg?alt=media&token=300f278f-2e30-4964-b875-84ddccfdb2b0'
+        },
+        {
+            name: 'Le Wagon',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Flewagon2.png?alt=media&token=4096d5dd-f51e-4c59-bcab-043a8eaeb500'
+        },
+        {
+            name: 'Karpatkey',
+            img: 'https://firebasestorage.googleapis.com/v0/b/ieeeitba.appspot.com/o/static%2Fkarpatkey.png?alt=media&token=6ed477da-b0ff-471d-8936-b40bec4c848d'
         }
     ];
 
